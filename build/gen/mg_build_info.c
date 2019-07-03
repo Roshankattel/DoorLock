4d94e08d3973b9b49274767cacadca95e25fca2e
Roshan Kattel <kattelroshan@gmail.com>
2019-07-03 15:18:15 +0545

    Added factory reset option

--- build/gen/mg_build_info.c
+++
@@ -1,4 +0,0 @@
-/* Auto-generated, do not edit. */
-const char *mg_build_id = "20190703-015852/2.14.0-gce86552";
-const char *mg_build_timestamp = "2019-07-03T01:58:52Z";
-const char *mg_build_version = "2.14.0";
--- fs/init.js
+++ fs/init.js
@@ -8,16 +8,26 @@ load('api_blynk.js');
 load('api_rpc.js');
 load('api_dash.js');
 
+let F_reset = ffi('void mgos_config_reset(int)');
+
 let ping = 0, reset_id, led;
 let relayPin = Cfg.get('hardware.relayPin');
 let relayStat = Cfg.get('hardware.relayStat'); //initial status of relay
 
 GPIO.setup_output(relayPin, relayStat);
 
+function unlock() {
+  GPIO.write(relayPin, 0);
+  print('Door lock open');
+  Timer.set(Cfg.get('hardware.pulseTm'), 0, function () {
+    print('Door lock closed');
+    GPIO.write(relayPin, 1);
+  }, null);
+}
+
 if (relayStat === false) {
   unlock();
 }
-
 
 Blynk.setHandler(function (conn, cmd, pin, val, id) {
   if (cmd === 'vr') {
@@ -36,11 +46,8 @@ RPC.addHandler('Wifi.Unlock', function () {
   unlock();
 });
 
-function unlock() {
-  GPIO.write(relayPin, 0);
-  print('Door lock open');
-  Timer.set(Cfg.get('hardware.pulseTm'), 0, function () {
-    print('Door lock closed');
-    GPIO.write(relayPin, 1);
-  }, null);
-}
+RPC.addHandler('Factory.reset', function () {
+  F_reset(9);
+  Sys.reboot(0);
+});
+
