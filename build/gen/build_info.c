af3faa219d6757c73a8f9e4ed9deba1c2e640f62
Roshan Kattel <kattelroshan@gmail.com>
2019-07-03 08:03:51 +0545

    optimized code With OTA updates

---
+++ build/gen/build_info.c
@@ -0,0 +1,4 @@
+/* Auto-generated, do not edit. */
+const char *build_id = "20190703-015852";
+const char *build_timestamp = "2019-07-03T01:58:52Z";
+const char *build_version = "1.0";
--- fs/init.js
+++ fs/init.js
@@ -8,18 +8,14 @@ load('api_blynk.js');
 load('api_rpc.js');
 load('api_dash.js');
 
-let ping = 0, reset_id,led;
+let ping = 0, reset_id, led;
 let relayPin = Cfg.get('hardware.relayPin');
 let relayStat = Cfg.get('hardware.relayStat'); //initial status of relay
 
 GPIO.setup_output(relayPin, relayStat);
 
 if (relayStat === false) {
-  print('Door lock open');
-  Timer.set(Cfg.get('hardware.pulseTm'), 0, function () {
-    print('Door lock closed');
-    GPIO.write(relayPin, 1);
-  }, null);
+  unlock();
 }
 
 
@@ -27,27 +23,24 @@ Blynk.setHandler(function (conn, cmd, pin, val, id) {
   if (cmd === 'vr') {
     ping = !ping;
     led = ping;
-    led = ping? 0:255;
-    Blynk.virtualWrite(conn, 1, led, id);    
+    led = ping ? 0 : 255;
+    Blynk.virtualWrite(conn, 1, led, id);
     Blynk.virtualWrite(conn, 0, ping, id);
-    
   }
   else if (cmd === 'vw' && pin === 5 && val === 1) {
-    // using virtual pin 5 to turn on 
-    print('Door lock open');
-    GPIO.write(relayPin, 0);
-    Timer.set(Cfg.get('hardware.pulseTm'), 0, function () {
-      print('Door lock closed');
-      GPIO.write(relayPin, 1);
-    }, null);
+    unlock();
   }
 }, null);
 
-RPC.addHandler('Unlock', function() {
+RPC.addHandler('Wifi.Unlock', function () {
+  unlock();
+});
+
+function unlock() {
   GPIO.write(relayPin, 0);
   print('Door lock open');
   Timer.set(Cfg.get('hardware.pulseTm'), 0, function () {
-  print('Door lock closed');
-  GPIO.write(relayPin, 1);
+    print('Door lock closed');
+    GPIO.write(relayPin, 1);
   }, null);
-});
+}
