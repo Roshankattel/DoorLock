ca4c5ac14b8e225bb3b3e4fe4c70c582d31c808d
Roshan Kattel <kattelroshan@gmail.com>
2020-01-13 13:19:38 +0545

    Added terminal in Blynk

--- fs/init.js
+++ fs/init.js
@@ -9,15 +9,19 @@ load('api_events.js');
 
 let F_reset = ffi('void mgos_config_reset(int)');
 
-let ping = 0, reset_id, led;
+let ping = 0, led;
 let relayPin = Cfg.get('hardware.relayPin');
 let relayStat = Cfg.get('hardware.relayStat'); //initial status of relay
+let reset = 0;
+let reset_id;
+let time;
 
 GPIO.setup_output(relayPin, relayStat);
 
 function unlock() {
   GPIO.write(relayPin, 0);
-  let time = Math.floor(Timer.now() * 1000);
+  let now = Timer.now();
+  time = Timer.fmt('%F %T', now);
   print('Door lock open at', time);
   Timer.set(Cfg.get('hardware.pulseTm'), 0, function () {
     print('Door lock closed');
@@ -33,13 +37,13 @@ if (relayStat === false) {
 Blynk.setHandler(function (conn, cmd, pin, val, id) {
   if (cmd === 'vr') {
     ping = !ping;
-    led = ping;
     led = ping ? 0 : 255;
     Blynk.virtualWrite(conn, 1, led, id);
     Blynk.virtualWrite(conn, 0, ping, id);
   }
   else if (cmd === 'vw' && pin === 5 && val === 1) {
     unlock();
+    Blynk.virtualWrite(conn, 4, time, id);
   }
 }, null);
 
@@ -53,6 +57,21 @@ RPC.addHandler('Factory.reset', function () {
 });
 
 Event.addHandler(Event.CLOUD_DISCONNECTED, function (ev, evdata, ud) {
-  print("cloud Disconnected,Going to reboot!"); 
-    Sys.reboot(0);
+  if (reset === 1) {
+    reset=reset+1;
+    let now = Timer.now();
+    let time = Timer.fmt('%F %T', now);
+    print("cloud Disconnected,Going to reboot!",time);
+    reset_id = Timer.set(300000, 0, function () {
+      Sys.reboot(0);
+    }, null); 
+  }
+}, null);
+
+Event.addHandler(Event.CLOUD_CONNECTED, function (ev, evdata, ud) {
+  if (reset>0){
+  print("cloud connected");
+  Timer.del(reset_id);
+  }
+  reset =1;
 }, null);
